0edbf8e93186a1b1338e72d9819c01eacd68e1cd
Samantha brake <sammy.brake@gmail.com>
2019-05-19 01:23:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ function BoardMember(name, homeState, training){
   this.doCharity = function(){
     return "I like to help people."
   }
-
-  this.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
-
-  this.sayHi = function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
 }
+  BoardMember.prototype.doCharity = function() {
+    return "I like to help people."
+  };
+  BoardMember.prototype.releasePressStatement = function(){
+    return "You will see great things from Scuber."
+  };
+  
+  BoardMember.prototype.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  };
+
+// User.prototype.sayHello = function() {
+//     console.log(`Hello everybody, my name is ${this.name}`);
+// };
