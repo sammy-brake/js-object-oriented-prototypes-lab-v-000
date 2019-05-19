62453dd21a8a240e13c2b42cf9533401cf51b39c
Samantha brake <sammy.brake@gmail.com>
2019-05-19 01:26:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training){
   this.training = training
 };
 
-  BoardMember.prototype.veto = function() 
+  BoardMember.prototype.veto = function()
     return 'No, I must disagree'
   };
   BoardMember.prototype.approve = function() {
